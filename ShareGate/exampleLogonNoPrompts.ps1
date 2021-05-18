2b598dd8dd76026f005e68cf696c61bee038c50a
tsmolskow <42013432+tsmolskow@users.noreply.github.com>
2021-05-18 16:28:05 -0400

    Add files via upload

---
+++ ShareGate/exampleLogonNoPrompts.ps1
@@ -0,0 +1,17 @@
+﻿#Logon Without a Prompt
+
+cls
+
+#Login and Password
+$login = "EYTestAdmin@M365B742352.onmicrosoft.com"
+$pwd = "Cognizantadmin123!"
+$pwd = ConvertTo-SecureString $pwd -AsPlainText -Force
+
+#Credentials
+$credentials = New-Object -TypeName System.Management.Automation.PSCredential -argumentlist $login,$pwd
+
+#Connect
+Connect-SPOService -url "https://m365b742352-admin.sharepoint.com" -Credential $credentials
+
+
+
---
+++ ShareGate/sgForEach.ps1
@@ -0,0 +1,10 @@
+﻿$csvFile = "C:\CSVfile.csv"
+
+$table = Import-Csv $csvFile -Delimiter ";"
+
+foreach ($row in $table)
+{
+	$srcSite = Connect-Site -Url $row.SourceSite
+	$dstPath = $row.DestinationPath
+	Export-Site -SourceSite $srcSite -Name "Important" -DestinationFolder $dstPath -Subsites
+}
