925ce4d2331a97bf0dfbbb19017a4a52e0f10d2e
tsmolskow <42013432+tsmolskow@users.noreply.github.com>
2021-06-29 14:41:23 +0000

    Add files via upload

---
+++ ShareGate/setAllListPermissions/spSetAllListsPermissions.ps1
@@ -0,0 +1,103 @@
+﻿# Set All SP On-Premise Lists Permissions
+# spSetAllListsPermissions
+# As of: 6/25/2021
+# Developer: Tom Molskow, Cognizant
+# https://social.technet.microsoft.com/wiki/contents/articles/31897.sharepoint-2013-powershell-automation-of-user-and-group-reconfigurations.aspx
+
+cls
+
+Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
+
+Set-SPSite -Identity "https://us.eyonespace.ey.com/Sites/8a7dc0780c504083b0021344954f7f37" -LockState "Unlock"
+
+# File Output Path - Modify This Path Variable to Reflect the Local Environment
+$filePath ="C:\Test\"
+$fileName = "eyimdUSA-0001677-MM_20210607084254-SPOnPrem.csv"
+
+# CSV List Input File - Modify This Path Variable to Reflect the Local Environment
+# !!!The 'spopPermissions.csv' File Must Exist Prior to Running!!!
+$csvFile = $filePath + $fileName
+
+# Table Variable
+$table = Import-Csv $csvFile -Delimiter ","
+
+# ID,fk_site,fk_batch,TypeOfContent,SiteName,SourceURL,Inheritance,UserGroup,Principaltype,Accountname,ContentPermissions
+
+    
+#try{
+
+    foreach ($row in $table)
+    {
+    
+    # Site Name
+    $siteName = $row.SiteName
+    Write-Host "Site Name: " $siteName 
+
+    # Site URL
+    $siteURL = $row.SourceURL 
+    Write-Host "Site URL: " $siteURL
+
+    # Site Inheritance
+    $groupInherit = $row.Inheritance
+    Write-Host "Inheritance: " $groupInherit
+
+    # User Group
+    $listGroup = $row.UserGroup
+    Write-Host "User Group: " $listGroup
+
+    # Account Name
+    $accountName = $row.Accountname
+    Write-Host "Account Name: " $accountName
+
+    # Permissions
+    $accountPermissions = ($row.ContentPermissions).Replace(";","")
+    Write-Host "Permissions: " $accountPermissions
+
+    Write-Host "--------------------------------------------------------------"    
+   
+    # Remove Permissions
+    $web = get-spweb $siteURL
+    Write-Host "Web: " $web #For Testing Only
+    $group = $web.SiteGroups[$accountName]
+    Write-Host "group: " $group #For Testing Only
+    $ra = $group.ParentWeb.RoleAssignments.GetAssignmentByPrincipal($group)
+    Write-Host "ra: " $ra #For Testing Only
+    $rd = $group.ParentWeb.RoleDefinitions["Read"]
+    Write-Host "rd: " $rd #For Testing Only
+    $ra.RoleDefinitionBindings.Remove($rd)
+    #$ra.RoleDefinitionBindings.Add($rd)
+    Write-Host "ra: " $ra #For Testing Only
+    $ra.Update()
+    $group.Update()
+    $web.Dispose()
+
+    Write-Host "Permissions Removed"
+    Write-Host "--------------------------------------------------------------"
+   
+    # Add Permissions
+    $web = get-spweb $siteURL
+    Write-Host "Web: " $web #For Testing Only
+    $group = $web.SiteGroups[$accountName]
+    Write-Host "group: " $group #For Testing Only
+    $ra = $group.ParentWeb.RoleAssignments.GetAssignmentByPrincipal($group)
+    Write-Host "ra: " $ra #For Testing Only
+    $rd = $group.ParentWeb.RoleDefinitions[$accountPermissions]
+    Write-Host "rd: " $rd #For Testing Only
+    #$ra.RoleDefinitionBindings.Remove($rd)
+    $ra.RoleDefinitionBindings.Add($rd)
+    Write-Host "ra: " $ra #For Testing Only
+    $ra.Update()
+    $group.Update()
+    $web.Dispose()
+
+    Write-Host "Permissions Added"
+    Write-Host "--------------------------------------------------------------"
+       
+    }
+#} catch {
+
+    #$ErrorMessage = $_.Exception.Message
+    #Write-Host "Error has occurred:" $ErrorMessage
+
+#}
+
---
+++ ShareGate/setAllListPermissions/spSetAllListsPermissionsCSOM2.ps1
@@ -0,0 +1,24 @@
+﻿# spSetAllListsPermissionsCSOM2
+
+cls
+
+Add-Type -Path "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll"
+Add-Type -Path "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll"
+
+#Proivde your details: SharePoint Site Url, UserName and Password   
+$SiteUrl="https://eyonespace.ey.com/Sites/536a5ebeec1044b0b8accf5c44547c41"
+$UserName = "P.AUOPRTMIGP.10@ey.com"
+$Password = 'cL95H@urF7~Vk3%'
+$SecPwd = $(ConvertTo-SecureString $Password -asplaintext -force)  
+
+#Connecting site web
+$ctx = New-Object Microsoft.SharePoint.Client.ClientContext($SiteUrl)  
+$credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($UserName,$SecPwd)  
+$ctx.credentials = $credentials
+$ctx.Load($ctx.Web) 
+$ctx.ExecuteQuery()
+ 
+#Find list by Title
+$list=$ctx.Web.Lists.GetByTitle("Documents") 
+$ctx.Load($list) 
+$ctx.ExecuteQuery()
